28c20f8615443110d0eefc45522edf93ce0b6499
megermer <113950667+megermer@users.noreply.github.com>
2023-03-28 18:28:02 -0400

    Adds relations to frontend

--- front.py
+++ front.py
@@ -57,37 +57,33 @@ st.write(submit_args)
 ###################
 
 # Creates Network class of specified size and styling
-net = Network(height='465px', bgcolor='#222222', font_color='white')
-
-# Test code##############################
-# net.add_node(1, label="Node 1") # node id = 1 and label = Node 1
-# net.add_node(2) # node id and label = 2
-# net.show("test.html")
-############################################
-
-# net.add_nodes(existing_node_names)
+net = Network(height='400px', bgcolor='#FFFFFF', font_color='black', directed=True)
 
 # Retrieves all of the three categories of nodes, needed for coloring issues
 characters = back.retrieve_all_nodes_of_label("Character")
 places = back.retrieve_all_nodes_of_label("Place")
 things = back.retrieve_all_nodes_of_label("Thing")
+relationships = back.retrieve_all_relationships()
 
 for character in characters:
-    net.add_node(character, color = "blue")
+    net.add_node(character, color = "#1261a0")
 
 for place in places:
-    net.add_node(place, color = "red")
+    net.add_node(place, color = "#ffb347")
 
 for thing in things:
-    net.add_node(thing, color = "yellow")
+    net.add_node(thing, color = "#acdf87")
+
+for relation in relationships:
+    net.add_edge(relation[0], relation[2], title=relation[1], color="grey")
 
 net.show("test.html")
 
 # CRUCIAL for displaying graph on server (try) and locally (except)
 try:
-   path = './tmp'
-   net.save_graph(f'{path}/pyvis_graph.html')
-   HtmlFile = open(f'{path}/pyvis_graph.html','r',encoding='utf-8')
+    path = './tmp'
+    net.save_graph(f'{path}/pyvis_graph.html')
+    HtmlFile = open(f'{path}/pyvis_graph.html','r',encoding='utf-8')
 except:
     path = './html_files'
     net.save_graph(f'{path}/pyvis_graph.html')
